a2375ea6863a9909dabd70d86f032988e37adffa
celiudos <marcelo.anselmo.souza@gmail.com>
2023-05-15 18:33:14 -0300

    fix: conexao com bd FBD

--- src/FBD/querys_sql/INSERT.sql
+++ src/FBD/querys_sql/INSERT.sql
@@ -1,20 +1,51 @@
- CREATE TABLE OCUPACAO (
-            id INT NOT NULL,
-            nome VARCHAR(255) NOT NULL,
-            PRIMARY KEY (id)
-        ) ENGINE=InnoDB;
+-- Active: 1684186307233@@127.0.0.1@3306@projfbd
 
- CREATE TABLE SEXO (
-            id INT NOT NULL,
-            nome VARCHAR(255) NOT NULL,
-            PRIMARY KEY (id)
-        ) ENGINE=InnoDB;
+-- TABELAS
 
- CREATE TABLE REGIAO (
-            id INT NOT NULL,
-            nome VARCHAR(255) NOT NULL,
-            PRIMARY KEY (id)
-        ) ENGINE=InnoDB;
+CREATE TABLE
+    OCUPACAO (
+        id INT NOT NULL,
+        nome VARCHAR(255) NOT NULL,
+        PRIMARY KEY (id)
+    ) ENGINE = InnoDB;
+
+CREATE TABLE
+    SEXO (
+        id INT NOT NULL,
+        nome VARCHAR(255) NOT NULL,
+        PRIMARY KEY (id)
+    ) ENGINE = InnoDB;
+
+CREATE TABLE
+    REGIAO (
+        id INT NOT NULL,
+        nome VARCHAR(255) NOT NULL,
+        PRIMARY KEY (id)
+    ) ENGINE = InnoDB;
 
 -- Drop all tables
 
+CREATE TABLE
+    UF (
+        id INT NOT NULL,
+        id_regiao INT NOT NULL,
+        nome VARCHAR(255) NOT NULL,
+        PRIMARY KEY (id),
+        FOREIGN KEY (id_regiao) REFERENCES REGIAO(id)
+    ) ENGINE = InnoDB;
+
+CREATE TABLE
+    RAIS (
+        id INT NOT NULL AUTO_INCREMENT,
+        id_ocupacao INT NOT NULL,
+        id_sexo INT NOT NULL,
+        id_uf INT NOT NULL,
+        ano INT NOT NULL,
+        remuneracao_media FLOAT NOT NULL,
+        desligamento INT NOT NULL,
+        idade INT NOT NULL,
+        PRIMARY KEY (id),
+        FOREIGN KEY (id_ocupacao) REFERENCES OCUPACAO(id),
+        FOREIGN KEY (id_sexo) REFERENCES SEXO(id),
+        FOREIGN KEY (id_uf) REFERENCES UF(id)
+    ) ENGINE = InnoDB;
